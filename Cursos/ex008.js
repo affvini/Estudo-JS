398a94f50b1e1a9e8bef6a2339369aedeb189146
affvini <vinicius.f.veras@gmail.com>
2022-08-03 17:44:08 -0300

    exercícios feitos em 03/08/2022

---
+++ Cursos/ex008.js
@@ -0,0 +1,8 @@
+var vel = 
+console.log(`A velocidade do seu carro é ${vel} km por hora.`)
+if (vel >60.0) { // Condição composta
+    console.log(`você ultrapassou a velocidade permitida. MULTADO`)
+}
+else (
+    console.log(`Dirija sempre usando cinto de segurança!`)
+)
